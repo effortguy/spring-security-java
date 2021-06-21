f176838043ec0d0da98737011998454afb6d577e
effortguy <xlcuqhdl@gmail.com>
2021-06-22 07:18:46 +0900

    인메모리 계정 사용

--- src/main/java/com/example/springsecurityjava/conf/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjava/conf/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecurityjava.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated();
         httpSecurity.formLogin();
         httpSecurity.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("keesun").password("{noop}123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }

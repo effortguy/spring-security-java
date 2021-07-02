1818791e129760b306b8dd0f8eaf2aaf6012aaa2
effortguy <xlcuqhdl@gmail.com>
2021-07-02 17:38:25 +0900

    WebSecurity Ignoring

--- src/main/java/com/example/springsecurityjava/conf/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjava/conf/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityjava.conf;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDecisionManager;
@@ -8,6 +9,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
@@ -26,12 +28,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .mvcMatchers("/", "/info", "/account").permitAll()
                     .mvcMatchers("/admin").hasRole("ADMIN")
                     .mvcMatchers("/user").hasRole("USER")
+                    // .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    // WebSecurity를 이용하는 게 더 좋다. filter를 다 타기 때문에 좋지 않다.
                     .anyRequest().authenticated()
                     .expressionHandler(defaultWebSecurityExpressionHandler());
 
         httpSecurity.formLogin();
 
         httpSecurity.httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        web.ignoring().requestMatchers(PathRequest.toH2Console());
+
+        // web.ignoring().mvcMatchers("/", "/info", "/account");
+        // 위와 같이 하면 로인한 사용자가 "/"로 왔을 때 권한 정보를 못 받아오기 때문에 좋지 않다.
     }
 
     private DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler() {
